d04e1b6071c1a5cb2d352da84f249e10e7e92247
Jswears <ji.swearssalinas@gmail.com>
2023-09-15 18:47:09 +0200

    fixing spotifyclient

--- src/services/spotifyClient.ts
+++ src/services/spotifyClient.ts
@@ -1,59 +1,92 @@
 /* eslint-disable no-useless-catch */
-import axios from "axios";
 import env from "../config/config";
 
-import SpotifyWebApi from "spotify-web-api-js";
-const spotifyApi = new SpotifyWebApi();
 const clientId = env.CLIENT_ID;
 const clientSecret = env.CLIENT_SECRET;
-
+const accessToken = localStorage.getItem("token");
 export const spotifyService = {
   retrieveToken: async () => {
     try {
-      const response = await axios.post(
-        "https://accounts.spotify.com/api/token",
-        "grant_type=client_credentials",
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: "Basic " + btoa(`${clientId}:${clientSecret}`),
-          },
-        }
-      );
-      spotifyApi.setAccessToken(response.data.access_token);
-      return response.data.acess_token;
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Basic " + btoa(`${clientId}:${clientSecret}`),
+        },
+        body: "grant_type=client_credentials",
+      });
+      const data = await response.json();
+      localStorage.setItem("token", data.access_token);
+      return data.acess_token;
     } catch (error) {
       throw error;
     }
   },
-  searchArtists: async (artist: string) => {
+  getUser: async () => {
     try {
-      const response = await spotifyApi.searchArtists(artist);
-      return response;
+      const response = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getArtistId: async (artist: string) => {
+    try {
+      const response = await fetch(
+        "https://api.spotify.com/v1/search?q=" + artist + "&type=artist",
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      const data = await response.json();
+      return data.artists.items[0].id;
     } catch (error) {
       throw error;
     }
   },
   getArtist: async (artistId: string) => {
     try {
-      const response = await spotifyApi.getArtist(artistId);
-      return response;
+      if (artistId) {
+        const response = await fetch(
+          `https://api.spotify.com/v1/artists/${artistId}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer " + accessToken,
+            },
+          }
+        );
+        const data = await response.json();
+        return data;
+      }
     } catch (error) {
       throw error;
     }
   },
-  getUser: async (user: string) => {
+  getArtistTopTracks: async (artistId: string) => {
     try {
-      const response = await spotifyApi.getUser(user);
-      return response;
-    } catch (error) {
-      throw error;
-    }
-  },
-  getTopTracks: async (id: string) => {
-    try {
-      const response = await spotifyApi.getArtistTopTracks(id, "GB");
-      return response;
+      const response = await fetch(
+        `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=US`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+      const data = await response.json();
+      if (data.tracks) {
+        return data.tracks.slice(0, 4);
+      }
     } catch (error) {
       throw error;
     }
