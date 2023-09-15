39790597badb43aa1f8b85bc72d9e1f0c41b1eb1
Jswears <ji.swearssalinas@gmail.com>
2023-09-15 18:49:29 +0200

    fix: usercontext

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,22 +1,19 @@
 import React, { createContext, useEffect, useState } from "react";
 import { spotifyService } from "../services/spotifyClient";
+import { UserProps } from "../types";
 type Props = {
   children: React.ReactNode;
 };
 
-const UserContext = createContext<SpotifyApi.UserProfileResponse | undefined>(
-  undefined
-);
+const UserContext = createContext<UserProps | undefined>(undefined);
 
 const UserContextWrapper = (props: Props) => {
-  const [user, setUser] = useState<SpotifyApi.UserProfileResponse | undefined>(
-    undefined
-  );
+  const [user, setUser] = useState<UserProps | undefined>(undefined);
 
   const fetchUserData = async () => {
     try {
       await spotifyService.retrieveToken();
-      const response = await spotifyService.getUser("j.swearssalinas");
+      const response = await spotifyService.getUser();
       setUser(response);
     } catch (error) {
       console.log(error);
